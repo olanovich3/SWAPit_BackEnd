194672ab9417be8cfc2e129a42dc1d20ad67b3ed
meryroo <meryrooprada@gmail.com>
2023-03-15 19:36:37 +0100

    feat products & users

--- src/api/routes/products.routes.js
+++ src/api/routes/products.routes.js
@@ -7,12 +7,12 @@ const {
   deleteProduct,
 } = require("../controllers/products.controller");
 const { upload } = require("../../middlewares/files.middleware");
-
+const { isAuth } = require("../../middlewares/auth.middleware");
 const ProductsRoutes = express.Router();
 
 ProductsRoutes.get("/", getAllProducts);
 ProductsRoutes.get("/:id", getProductByID);
-ProductsRoutes.post("/", upload.single("images"), createProduct);
+ProductsRoutes.post("/", [isAuth], upload.single("images"), createProduct);
 ProductsRoutes.patch("/:id", upload.single("images"), updateProduct);
 ProductsRoutes.delete("/:id", deleteProduct);
 
