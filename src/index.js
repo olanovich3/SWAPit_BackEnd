169c3928f5b42a471dad841a140082ca09821720
meryroo <meryrooprada@gmail.com>
2023-03-19 18:59:35 +0100

    feat headers

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ const server = express();
 //connect to DB
 connect();
 
+//HEADERS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 //CORS
 server.use(
   cors({
