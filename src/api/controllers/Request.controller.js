5b982b5267f798e6ffeaf39cd58ceb0f422c3ef2
meryroo <meryrooprada@gmail.com>
2023-03-28 11:15:07 +0200

    request form

---
+++ src/api/controllers/Request.controller.js
@@ -0,0 +1,54 @@
+const Request = require("../models/Request.model");
+const Product = require("../models/product.model");
+
+const getAllRequest = async (req, res, next) => {
+  try {
+    const request = await Request.find().populate("userfrom userto product");
+    return res.status(200).json(request);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const createRequest = async (req, res, next) => {
+  try {
+    const { product } = req.params;
+    const solicitedProduc = await Product.findById(product);
+    const owner = solicitedProduc.owner._id;
+    const newRequest = new Request({
+      userfrom: req.user._id,
+      userto: owner,
+      product: product,
+    });
+    const createdRequest = await newRequest.save();
+    return res.status(200).json(createdRequest);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const HandleRequest = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+    const updatedRequest = await Request.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+
+    if (!updatedRequest) {
+      return res.status(404).json({ error: "Solicitud no encontrada" });
+    }
+
+    return res.status(200).json(updatedRequest);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = {
+  createRequest,
+  HandleRequest,
+  getAllRequest,
+};
---
+++ src/api/routes/Request.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { isAuth } = require("../../middlewares/auth.middleware");
+
+const {
+  createRequest,
+  HandleRequest,
+  getAllRequest,
+} = require("../controllers/Request.controller");
+
+const RequestRoutes = express.Router();
+
+RequestRoutes.post("/:product", [isAuth], createRequest);
+RequestRoutes.get("/", getAllRequest);
+RequestRoutes.put("/:id", [isAuth], HandleRequest);
+
+module.exports = RequestRoutes;
